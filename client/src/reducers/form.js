b0d054305640b1b3075fb197a58b75bd904154f2
melonattacker <>
2019-07-11 10:36:28 +0900

    modify form reducer

--- client/src/reducers/form.js
+++ client/src/reducers/form.js
@@ -1,11 +1,17 @@
-import { CHANGE_IMAGE, INITIALIZE_FORM } from '../actions';
+import { CHANGE_TEXT, CHANGE_IMAGE, INITIALIZE_FORM } from '../actions';
 
 const initialState = {
+    text: '',
     image: null
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case CHANGE_TEXT:
+            return {
+                ...state,
+                image: action.text
+            }
         case CHANGE_IMAGE:
             return {
                 ...state,
