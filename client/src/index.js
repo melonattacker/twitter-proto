24b8cf9fb6769bf437534a624910ed1f3c8f752f
melonattacker <>
2019-07-11 15:40:09 +0900

    add react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { createStore } from 'redux'
 import App from './components/App'
 import rootReducer from './reducers'
 import { Provider } from 'react-redux'
+import {BrowserRouter,Route,Switch} from "react-router-dom"
 
 const store = createStore(rootReducer);
 
@@ -11,7 +12,11 @@ console.log(store.getState());
 
 ReactDOM.render (
   <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={App} />
+        </Switch>
+      </BrowserRouter>
   </Provider>
   ,document.getElementById('root')
 )
