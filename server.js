9cde8083ab905ed79d74fc0fdf38072db880b0d9
melonattacker <>
2019-07-12 13:59:29 +0900

    add setHeader to response

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors);
 
+const ALLOWED_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'HEAD',
+    'OPTIONS'
+];
+
 const client = mysql.createConnection({
     host: 'us-cdbr-iron-east-02.cleardb.net',
     user: 'b6f26e95edb042',
@@ -23,45 +33,57 @@ client.connect(function (err) {
 
 // 画像一括取得
 app.get('/post', (req, res) => {
+    const origin = req.headers.origin;
     client.query('SELECT * from post;', (err, rows, fields) => {
         if (err) throw err;
-        res.setHeader('Access-Controll-Allow-Origin');
+        res.setHeader('Access-Controll-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', ALLOWED_METHODS.join(','));
+        res.setHeader('Access-Control-Allow-Headers', 'Content-type,Accept,X-Custom-Header');
         res.send(rows);
     });
 });
 
 // ユーザーごとの画像を取得
 app.post('/post/user', (req, res) => {
+    const origin = req.headers.origin;
     const created_by = req.body.created_by;
     client.query('SELECT * from post WHERE created_by = ?;', [created_by], (err, rows, fields) => {
         if (err) throw err;
-        res.setHeader('Access-Controll-Allow-Origin');
+        res.setHeader('Access-Controll-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', ALLOWED_METHODS.join(','));
+        res.setHeader('Access-Control-Allow-Headers', 'Content-type,Accept,X-Custom-Header');
         res.send(rows);
     });
 });
 
 // 画像のパスを保存
 app.post('/post/create', (req, res) => {
+    const origin = req.headers.origin;
     const created_by = req.body.created_by;
     const text = req.body.text;
     const image_url = req.body.image_url;
     const time = new Date();
     client.query('INSERT INTO post SET ?', { created_by: created_by, text: text, image_url: image_url, time: time }, (err, result) => {
         if (err) throw err;
-        res.setHeader('Access-Controll-Allow-Origin');
+        res.setHeader('Access-Controll-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Methods', ALLOWED_METHODS.join(','));
+        res.setHeader('Access-Control-Allow-Headers', 'Content-type,Accept,X-Custom-Header');
         res.send(result);
     })
 });
 
 // 画像のパスを削除
 app.delete('/post/delete', (req, res) => {
+    const origin = req.headers.origin;
     const id = req.body.id;
     const created_by = req.body.created_by;
     client.query(`DELETE FROM post WHERE id = ?`, [id], (err, result) => {
         if (err) throw err;
         client.query('SELECT * from post WHERE created_by = ?', [created_by], (err, rows, fields) => {
             if (err) throw err;
-            res.setHeader('Access-Controll-Allow-Origin');
+            res.setHeader('Access-Controll-Allow-Origin', origin);
+            res.setHeader('Access-Control-Allow-Methods', ALLOWED_METHODS.join(','));
+            res.setHeader('Access-Control-Allow-Headers', 'Content-type,Accept,X-Custom-Header');
             res.send(rows);
         });
     });
