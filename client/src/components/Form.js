4b53ca31e98cfba537bd6bc8cd081eb5ae8bf20e
melonattacker <>
2019-07-12 13:24:08 +0900

    modify endpoint

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import firebase from '../firebase/firebase';
 import axios from 'axios';
 
-const ROOT_ENDPOINT = 'http://localhost:3001';
+const ROOT_ENDPOINT = 'mysql://b6f26e95edb042:04f7ce59@us-cdbr-iron-east-02.cleardb.net/heroku_06f4641160041ce?reconnect=true';
 
 const Form = ({ uid, text, image, changeText, changeImage, initializeForm }) => {
     const postTweet = () => {
